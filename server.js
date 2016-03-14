20b58381092e88bb413ac95c7aa64cf08bd6a759
Pankov_a <pankov.oleksandr@gmail.com>
2016-03-14 16:06:22 +0200

    connecting to mlab

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 import express from "express";
 
+import {MongoClient} from "mongodb"
+
+require('dotenv').config();
+
 let app = express();	
 
 app.use(express.static('public'));
 
 const server = app.listen(8080, () => {
 	console.log("Server is listening " + server.address().port);
+});
+
+MongoClient.connect(process.env.MONGO_URL, (err, database) => {
+	if (err) throw err;
+
+	database.collection("links").find({}).toArray((err, links) => {
+		if (err) throw err;
+
+		console.log(links);
+	})
 })
 
