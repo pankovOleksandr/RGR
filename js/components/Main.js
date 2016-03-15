27dabeec35d1b6a84a7a7b2566642d4b7d92e856
Pankov_a <pankov.oleksandr@gmail.com>
2016-03-15 14:01:19 +0200

    implementing stores and event

--- js/components/Main.js
+++ js/components/Main.js
@@ -1,17 +1,43 @@
 import React from "react";
 import API from "../API";
+import LinkStore from "../stores/LinkStore";
+
+let _getAppState = () => {
+	 return {links: LinkStore.getAll() };
+}
 
 export default class Main extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = _getAppState();
+		this.onChange = this.onChange.bind(this);
+	}
 	componentDidMount() {
 		API.fetchLinks();
+		LinkStore.on("change", this.onChange)
+	}
+	componentWillUnmount() {
+		LinkStore.removeListener("change", this.onChange);
+	}
+	onChange() {
+		console.log("4. In the view");
+		this.setState(_getAppState());
 	}
 	render() {
+		let attr = {
+			target: "_blank"
+		};
+		let content = this.state.links.map(link => {
+			return <li key={link._id}>
+						<a target={attr.target} href={link.url}>{link.title}</a>
+					</li>;
+		})
 		return (
 			<div>
 				<h3>Links</h3>
 				<ul>
-					<li>Link ...</li>
-					<li>Link ...</li>
+					{content}
 				</ul>	
 			</div>
 		);
